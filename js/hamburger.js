7910502debe217c17ea585bacc5331f9865acb0f
Derrick Holleman <derrickholleman@gmail.com>
2021-04-28 02:19:31 -0500

    inital commit

---
+++ js/hamburger.js
@@ -0,0 +1,28 @@
+/* hamburger menu animation toggle */
+$(document).ready(function(){
+	$('#hamburger').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+/* hamburger menu open */
+/* when you click on the element with an ID of hamburger, the class that has .is-active after it is toggled on */ 
+/* when .mobile-nav is selected the .is-active class gets toggled on */
+const menu_btn = document.getElementById('hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+menu_btn.addEventListener('click', function () {
+	menu_btn.classList.toggle('is-active');
+	mobile_menu.classList.toggle('is-active');
+})
+
+/* locks the hamburger menu from scrolling while open */
+jQuery('#hamburger').click(function(){
+    var menu_active = jQuery(this).hasClass('is-active');
+    if(menu_active){
+        jQuery('body').css('position','fixed');
+    } 
+    else {
+       jQuery('body').css('position','static');
+    }
+});
